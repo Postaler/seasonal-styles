d548fb38c51a6d731a39e4489bbb3024c5032290
Andrei Novozhilov <andreinovozhilov15@gmail.com>
2025-04-16 14:38:37 -0700

    done

--- js/start-here.js
+++ js/start-here.js
@@ -1,7 +1,27 @@
-$("document").ready(function(){
-    $('.seasons a').click(function(e){//find all a tags inside class of seasons
-        e.preventDefault();//stop default submission
-        var season = $(this).attr("href");//contents of href attribute of this element
-        alert(season);
+$(document).ready(function () {
+    console.log("start-here.js loaded ✅");
+
+    $('.seasons a').click(function (e) {
+        e.preventDefault();
+
+        const season = $(this).attr("href").toLowerCase();
+        console.log("Season clicked:", season);
+
+        $('link[rel="stylesheet"]').first().attr('href', `css/${season}.css`);
+
+        $('#logo').attr('src', `images/${season}.gif`);
+
+        
+        $('#wear').attr('src', `images/${season}-wear.jpg`);
+
+        
+        const slogans = {
+            spring: "Leap into our Spring Wear!",
+            summer: "Sizzle in Summer Style!",
+            fall: "Fall into Fashion!",
+            winter: "Wrap Up with Winter Warmth!"
+        };
+
+        $('h3').text(slogans[season] || "Outfitter for All Seasons!");
     });
 });
